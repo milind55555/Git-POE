3da94d29b4f1277a51777d0e197bfba8d7ef5a3e
Milind Hanchate <157528508+milind55555@users.noreply.github.com>
2025-10-28 11:35:34 +0530

    Fix formatting of new message output

--- app.java
+++ app.java
@@ -1,7 +1,7 @@
 public class app {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-        System.out.println("New Message added!!!");
+        System.out.println("New Message added!   !!");
        
     }
 }
