1cdc9a4d303afc5d2440926e8003876927cfec97
Milind Hanchate <157528508+milind55555@users.noreply.github.com>
2025-10-28 11:32:45 +0530

    h1 aded ssage to main method

--- app.java
+++ app.java
@@ -2,5 +2,6 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("New Message added!!!");
+         System.out.println("h1 added!!!");
     }
 }
